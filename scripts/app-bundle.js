c44d9da1f0bee3db3c200ba89ebe48103d2269d6
eegzapp <mike.egan@sandisk.com>
2016-12-07 17:12:46 -0800

    Added working d3 code - Created a simple crappy horizontal bar chart - Used self referencing (ref property) to contain chart to aurelia custom element 'my-d3'

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -67,6 +67,89 @@ define('main',['exports', './environment'], function (exports, _environment) {
     });
   }
 });
+define('my-d3',['exports', 'aurelia-framework', 'd3'], function (exports, _aureliaFramework, _d) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.MyD3 = undefined;
+
+  function _initDefineProp(target, property, descriptor, context) {
+    if (!descriptor) return;
+    Object.defineProperty(target, property, {
+      enumerable: descriptor.enumerable,
+      configurable: descriptor.configurable,
+      writable: descriptor.writable,
+      value: descriptor.initializer ? descriptor.initializer.call(context) : void 0
+    });
+  }
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) {
+    var desc = {};
+    Object['ke' + 'ys'](descriptor).forEach(function (key) {
+      desc[key] = descriptor[key];
+    });
+    desc.enumerable = !!desc.enumerable;
+    desc.configurable = !!desc.configurable;
+
+    if ('value' in desc || desc.initializer) {
+      desc.writable = true;
+    }
+
+    desc = decorators.slice().reverse().reduce(function (desc, decorator) {
+      return decorator(target, property, desc) || desc;
+    }, desc);
+
+    if (context && desc.initializer !== void 0) {
+      desc.value = desc.initializer ? desc.initializer.call(context) : void 0;
+      desc.initializer = undefined;
+    }
+
+    if (desc.initializer === void 0) {
+      Object['define' + 'Property'](target, property, desc);
+      desc = null;
+    }
+
+    return desc;
+  }
+
+  function _initializerWarningHelper(descriptor, context) {
+    throw new Error('Decorating class property failed. Please ensure that transform-class-properties is enabled.');
+  }
+
+  var _desc, _value, _class, _descriptor;
+
+  var MyD3 = exports.MyD3 = (_class = function () {
+    function MyD3() {
+      _classCallCheck(this, MyD3);
+
+      _initDefineProp(this, 'myData', _descriptor, this);
+
+      this.myData = "X";
+    }
+
+    MyD3.prototype.attached = function attached() {
+
+      (0, _d.select)(this.thisChart).selectAll('div').data(this.myData).enter().append('div').style('width', function (d) {
+        return d * 10 + 'px';
+      }).style('background-color', 'red').text(function (elm) {
+        return elm;
+      });
+    };
+
+    return MyD3;
+  }(), (_descriptor = _applyDecoratedDescriptor(_class.prototype, 'myData', [_aureliaFramework.bindable], {
+    enumerable: true,
+    initializer: null
+  })), _class);
+});
 define('spam-spam',['exports', 'aurelia-framework'], function (exports, _aureliaFramework) {
   'use strict';
 
@@ -146,6 +229,7 @@ define('resources/index',["exports"], function (exports) {
   exports.configure = configure;
   function configure(config) {}
 });
-define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <require from=\"spam-spam\"></require>\n\n  <h1 css.bind=\"{color:'red'}\">${message}</h1>\n\n  <spam-spam message.bind=\"'replacement text'\"></spam-spam>\n\n</template>\n"; });
+define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <require from=\"spam-spam\"></require>\n  <require from=\"my-d3\"></require>\n\n  <h1 css.bind=\"{color:'red'}\">${message}</h1>\n\n  <spam-spam message.bind=\"'replacement text'\"></spam-spam>\n\n  <my-d3 my-data.bind=\"[0,1,2,3,4,5,6,7]\"></my-d3>\n  <my-d3 my-data.bind=\"['A','B','C']\"></my-d3>\n  <my-d3 my-data.bind=\"[{x:0,y:1}, {x:20,y:30}]\"></my-d3>\n\n</template>\n"; });
+define('text!my-d3.html', ['module'], function(module) { module.exports = "<template>\r\n    <div ref=\"thisChart\">\r\n    </div>\r\n</template>"; });
 define('text!spam-spam.html', ['module'], function(module) { module.exports = "<template>\r\n    <button>${message}</button>\r\n</template>"; });
 //# sourceMappingURL=app-bundle.js.map
---
+++ src/my-d3.js
@@ -0,0 +1,36 @@
+
+import { bindable } from 'aurelia-framework';
+import { select, timeFormat } from 'd3';
+
+export class MyD3 {
+  @bindable myData;
+
+  constructor() {
+    this.myData = "X";
+  }
+
+  attached() {
+
+/**
+ * Draw a bar chart with the width of each bar set by each element passed in 
+ */
+
+    select(this.thisChart) // grab the chart div
+      .selectAll('div')  // select all div children (should be empty)
+        .data(this.myData)  // associate data with the selection. This is weirdness. If there's 8 pieces of data in this.myData, then 8 enter() events will be registered
+      .enter().append('div')  // handle the 8 enter events. In this case, we're appending a 'div' for each...
+        .style('width', d => d*10+'px')  // Then setting it's style...
+        .style('background-color', 'red')
+        .text((elm) => elm);  // And width (because they're bars in a horizontal bar chart)
+
+        /*
+        var formatMonth = timeFormat("%B"),
+            formatDay = timeFormat("%A"),
+            date = new Date(2014, 4, 1); 
+
+            console.log(formatMonth(date)); // "May"
+            console.log(formatDay(date)); // "Thursday"
+            */
+      }    
+}
+
